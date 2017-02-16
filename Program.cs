abff56cf3cb170216923cee8e50ecc50045bdde0
Richard Case <richard.case@outlook.com>
2017-02-16 10:42:19 +0000

    Initial checkin
    Signed-off-by: Richard Case <richard.case@outlook.com>

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Loader;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AssemblyLoadContext.Default.Unloading += UnloadOnSigTerm;
+
+            Log("Testing SIGTERM.");
+            while(true)
+            {
+                Log("Sleeping for 1 seconds");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void UnloadOnSigTerm(AssemblyLoadContext context)
+        {
+            Log("In unload. Sleeping 10 seconds before shutting down");
+            Thread.Sleep(10000);
+            Log("Finished sleeping exiting UnloadOnSIgTerm");
+        }
+
+        private static void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
